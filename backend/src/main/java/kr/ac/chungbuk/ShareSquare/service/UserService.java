a98654a5c9440a1f02bfc92f280cdb1526eff1db
Woochang Lee <changi112242@outlook.com>
2022-11-12 15:31:25 +0900

    UB Add Javadoc to methods

--- backend/src/main/java/kr/ac/chungbuk/ShareSquare/service/UserService.java
+++ backend/src/main/java/kr/ac/chungbuk/ShareSquare/service/UserService.java
@@ -32,6 +32,11 @@ public class UserService implements UserDetailsService {
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * @param username
+     * @return username가 받은 값인 user를 찾아 User(UserDetails의 자식 클래스) 객체를 반환합니다.
+     * @throws UsernameNotFoundException
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
@@ -39,12 +44,24 @@ public class UserService implements UserDetailsService {
         return user;
     }
 
+    /**
+     * @param id
+     * @return id가 받은 값인 user를 찾아 User(UserDetails의 자식 클래스) 객체를 반환합니다.
+     * @throws UsernameNotFoundException
+     */
     public UserDetails loadUserById(Long id) throws UsernameNotFoundException {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new UsernameNotFoundException("User not present"));
         return user;
     }
 
+
+    /**
+     * user를 생성합니다.
+     * @param username
+     * @param password
+     * @param email
+     */
     public void create(String username, String password, String email) throws Exception
     {
         if (!userRepository.existsByUsername(username) && !username.equals("anonymousUser")) {
@@ -67,6 +84,14 @@ public class UserService implements UserDetailsService {
         }
     }
 
+    /**
+     * 주어진 username인 user를 찾아 password가 일치하면 newPassword(선택적)와 email을 수정합니다.
+     * @param username 필수
+     * @param password 기존 비밀번호 필수
+     * @param newPassword 새 비밀번호(선택적)
+     * @param email 필수
+     * @throws Exception
+     */
     public void update(String username, String password, String newPassword, String email) throws Exception
     {
         User user = userRepository.findByUsername(username)
@@ -84,6 +109,11 @@ public class UserService implements UserDetailsService {
         }
     }
 
+    /**
+     * 주어진 username인 user를 삭제합니다.
+     * @param username
+     * @throws Exception
+     */
     public void delete(String username) throws Exception {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not present"));
@@ -93,6 +123,15 @@ public class UserService implements UserDetailsService {
         userRepository.save(user);
     }
 
+
+    /**
+     * username과 password로 로그인을 시도합니다. 100회 실패시 로그인이 잠깁니다.
+     * 성공시 엑세스 토큰을 반환합니다.
+     * @param username
+     * @param password
+     * @return token 반환
+     * @throws UsernameNotFoundException username 존재하지 않는 경우, user가 밴 또는 잠금 또는 삭제된 경우
+     */
     public String tryLogin(String username, String password) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not present"));
@@ -123,15 +162,28 @@ public class UserService implements UserDetailsService {
         return token;
     }
 
+    /**
+     * username 중복을 확인하여 사용할 수 있는지 반환합니다.
+     * @param username
+     * @return 사용할 수 있으면 true, 없으면 false
+     */
     public boolean canUseAsUsername(String username) {
         return !userRepository.existsByUsername(username) && !username.equals("anonymousUser");
     }
 
+    /**
+     * user를 로그인할 수 없도록 잠급니다.
+     * @param user
+     */
     public void lock(User user) {
         user.setLocked(true);
         userRepository.save(user);
     }
 
+    /**
+     * user를 로그인할 수 없도록 밴 합니다.
+     * @param user
+     */
     public void ban(User user) {
         user.setBanned(true);
         userRepository.save(user);
