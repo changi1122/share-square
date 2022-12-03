a5fdca364072c647d58ee9305a96d32f2da34d6b
sori9899 <hyun989969@gmail.com>
2022-12-03 20:09:33 +0900

    B Fix Share/Comment Repositiory Capital to lower case

--- backend/src/main/java/kr/ac/chungbuk/ShareSquare/repository/CommentRepository.java
+++ backend/src/main/java/kr/ac/chungbuk/ShareSquare/repository/CommentRepository.java
@@ -13,16 +13,16 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query(nativeQuery = true, value ="SELECT c.id, c.content, c.created_at, c.is_child, c.parent_id, c.community_id, u.username, u.profile_image FROM Comment c INNER JOIN Users u on c.user_id = u.id WHERE c.is_deleted = false AND c.community_id = :id ORDER BY c.parent_id asc , c.created_at asc;" )
+    @Query(nativeQuery = true, value ="SELECT c.id, c.content, c.created_at, c.is_child, c.parent_id, c.community_id, u.username, u.profile_image FROM comment c INNER JOIN users u on c.user_id = u.id WHERE c.is_deleted = false AND c.community_id = :id ORDER BY c.parent_id asc , c.created_at asc;" )
     List<CommentInterface> searchAllByCommunity_idAndIs_deletedFalse(@Param("id") Long id);
 
-    @Query(nativeQuery = true, value = "SELECT DISTINCT c.community_id from Comment c WHERE c.is_deleted = false AND :uid = c.user_Id ORDER BY c.created_at DESC ;")
+    @Query(nativeQuery = true, value = "SELECT DISTINCT c.community_id from comment c WHERE c.is_deleted = false AND :uid = c.user_Id ORDER BY c.created_at DESC ;")
     List<Long> searchCommentByUser_id(@Param("uid") Long uid);
 
-    @Query(nativeQuery = true, value = "SELECT COUNT(*) FROM Comment c where c.is_deleted = false AND c.community_id = :id ;")
+    @Query(nativeQuery = true, value = "SELECT COUNT(*) FROM comment c where c.is_deleted = false AND c.community_id = :id ;")
     Integer Number_of_ROW(@Param("id")Long id);
 
-    @Query(nativeQuery = true, value = "SELECT c.parent_id from Comment c WHERE c.id != :id AND c.parent_id LIKE :id AND c.is_deleted = false;")
+    @Query(nativeQuery = true, value = "SELECT c.parent_id from comment c WHERE c.id != :id AND c.parent_id LIKE :id AND c.is_deleted = false;")
     List<Long> IsExistAtParent_Id(@Param("id")Long id);
 
 }
--- backend/src/main/java/kr/ac/chungbuk/ShareSquare/repository/CommunityRepository.java
+++ backend/src/main/java/kr/ac/chungbuk/ShareSquare/repository/CommunityRepository.java
@@ -12,19 +12,19 @@ import java.util.List;
 @Repository
 public interface CommunityRepository extends JpaRepository<Community,Long>, JpaSpecificationExecutor<Community> {
 
-    @Query(nativeQuery = true, value= "SELECT * FROM Community m WHERE m.id > :id and m.is_deleted = false ORDER BY m.id ASC LIMIT 1")
+    @Query(nativeQuery = true, value= "SELECT * FROM community m WHERE m.id > :id and m.is_deleted = false ORDER BY m.id ASC LIMIT 1")
     List<Community> findNext(@Param("id") Long id);
 
-    @Query(nativeQuery = true, value = "SELECT * FROM Community m WHERE m.id < :id and m.is_deleted = false ORDER BY m.id DESC LIMIT 1; ")
+    @Query(nativeQuery = true, value = "SELECT * FROM community m WHERE m.id < :id and m.is_deleted = false ORDER BY m.id DESC LIMIT 1; ")
     List<Community> findPrevios(@Param("id") Long id);
 
-    @Query(nativeQuery = true, value= "SELECT * FROM Community m WHERE m.id > :id and m.is_deleted = false ORDER BY m.id ASC LIMIT 1")
+    @Query(nativeQuery = true, value= "SELECT * FROM community m WHERE m.id > :id and m.is_deleted = false ORDER BY m.id ASC LIMIT 1")
     Community Next(@Param("id") Long id);
 
-    @Query(nativeQuery = true, value = "SELECT * FROM Community m WHERE m.id < :id and m.is_deleted = false ORDER BY m.id DESC LIMIT 1; ")
+    @Query(nativeQuery = true, value = "SELECT * FROM community m WHERE m.id < :id and m.is_deleted = false ORDER BY m.id DESC LIMIT 1; ")
     Community Previos(@Param("id") Long id);
 
-    @Query(nativeQuery = true, value = "SELECT * from Community m WHERE m.user_id = :id and m.is_deleted =false ORDER BY m.created_at DESC ;")
+    @Query(nativeQuery = true, value = "SELECT * from community m WHERE m.user_id = :id and m.is_deleted =false ORDER BY m.created_at DESC ;")
     List<Community> SelectByUserId(@Param("id") Long id);
 
 
